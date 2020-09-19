d17f8fa4d8edeae6359c8318ecf12a3d28a24752
marwin1991 <peter.zmilczak@gmail.com>
2020-09-19 14:12:16 +0200

    Implmemented draw_ractangle

--- embroidery.py
+++ embroidery.py
@@ -1,5 +1,21 @@
-def draw_rectangle(width, height):
+def draw_rectangle(width, height, border_width=1, border_color=1, fill_color=1):
     matrix = []
+
+    for i in range(height):
+        row_list = []
+
+        if i < border_width or i >= height - border_width:
+            for j in range(width):
+                row_list.append(border_color)
+            matrix.append(row_list)
+        else:
+            for j in range(width):
+                if j < border_width or j >= width - border_width:
+                    row_list.append(border_color)
+                else:      
+                    row_list.append(fill_color)
+            matrix.append(row_list)
+
     return matrix
 
 
@@ -28,7 +44,12 @@ def embroider(matrix, color_scheme):
 
 if __name__ == '__main__':
     color_scheme = {0: ' ', 1: '*', 2: '.'}
-    embroider([[0, 0, 0, 1, 0, 0, 0], [0, 0, 1, 2, 1, 0, 0], [0, 1, 2, 2, 2, 1, 0], [1, 1, 1, 1, 1, 1, 1]], color_scheme)
+    # embroider([
+    #     [0, 0, 0, 1, 0, 0, 0], 
+    #     [0, 0, 1, 2, 1, 0, 0], 
+    #     [0, 1, 2, 2, 2, 1, 0], 
+    #     [1, 1, 1, 1, 1, 1, 1]], color_scheme)
 
     # This should have the same output:
-    # embroider(draw_triangle(4, border_color=1, fill_color=2), color_scheme)
+    my_border_color = 3
+    embroider(draw_rectangle(9, 10, border_width=2,border_color=2, fill_color=0), color_scheme)
